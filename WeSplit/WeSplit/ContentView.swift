28812e0994d65df3118269b5a9032b29eea9dabf
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-14 21:56:39 +0600

    feat: Picker

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,10 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State  private var selectedStudent = "Harry"
+    
     var body: some View {
-        Form {
-            ForEach(0 ..< 100) {
-                Text("Row \($0)")
+        Picker("Select your student", selection: $selectedStudent) {
+            ForEach(0 ..< students.count) {
+                Text(self.students[$0])
             }
         }
     }
