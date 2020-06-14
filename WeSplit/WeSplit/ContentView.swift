6685f2a1567874fc8d0edf9f42b7f38a7c3d7a2a
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-14 21:03:23 +0600

    feat: add Form with NavigationView

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, World!")
+                }
+            }
+            .navigationBarTitle(Text("SwiftUI"))
+        }
     }
 }
 
