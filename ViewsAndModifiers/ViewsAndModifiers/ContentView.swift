39b192aab6f32bc3cab49766fa216846903cb0ff
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-20 22:03:09 +0600

    feat(day 23): ViewsAndModifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Beknar Danabek on 6/20/20.
+//  Copyright © 2020 Beknar Danabek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .padding()
+            .background(Color.red)
+            .padding()
+            .background(Color.green)
+            .padding()
+            .background(Color.yellow)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
