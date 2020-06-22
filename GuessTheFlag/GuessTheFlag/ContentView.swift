d7b0a4ceadb65ed125ed494dec9848cf3cdeb9d2
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-22 18:49:39 +0600

    feat(day 24): challenge 3

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,6 +8,18 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    let imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.white, lineWidth: 1))
+            .shadow(color: .black, radius: 2)
+    }
+}
+
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland",
                              "Italy", "Nigeria", "Poland", "Russia",
@@ -39,11 +51,7 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.white, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                        FlagImage(imageName: self.countries[number])
                     }
                 }
                 Text("Your score is \(score)")
