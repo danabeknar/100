7941938b177113cb9b6f726b5763fa3647d1d0ad
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-24 21:26:39 +0600

    feat(day 26): selecting dates and times with DatePicker

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,12 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
     
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-            Text("\(sleepAmount, specifier: "%.2g") hours")
-        }
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date()...)
+            .labelsHidden()
     }
 }
 
