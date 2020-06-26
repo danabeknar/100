9d5a3199b6417d0a40887b08a02155989d1afd1f
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-26 18:44:52 +0600

    feat(day 32): Animation: Introduction

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Beknar Danabek on 6/26/20.
+//  Copyright © 2020 Beknar Danabek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
