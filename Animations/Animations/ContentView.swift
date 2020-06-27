c2fda64102a140721d3cd53eae7ba0d09af96275
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-27 20:57:00 +0600

    feat(day 33): controlling the animation stack

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,19 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         Button("Tap me") {
             withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
+                self.enabled.toggle()
             }
         }
-        .padding(50)
-        .background(Color.red)
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(nil)
         .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 0))
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
     }
 }
 
