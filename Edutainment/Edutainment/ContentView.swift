2cc3d17dba3feee03f8810821b6242e00be454b8
Beknar Danabek <beknar.danabek@gmail.com>
2020-06-29 23:29:29 +0600

    feat(day 35): add questions count picker

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -11,20 +11,32 @@ import SwiftUI
 struct ContentView: View {
     @State private var multiplicationTable = 1
     
+    @State private var questions = ["5", "10", "15", "All"]
+    @State private var selectedQuestionsCount = 0
+    
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Select multiplication table")) {
+                Section(header: Text("Select multiplication table:")) {
                     Picker("", selection: $multiplicationTable) {
-                        ForEach (1..<11) { number in
-                            Text("\(number)")
+                        ForEach (1..<11) { index in
+                            Text("\(index)")
                         }
                     }
-                .pickerStyle(WheelPickerStyle())
-                .labelsHidden()
+                    .pickerStyle(WheelPickerStyle())
+                    .labelsHidden()
+                }
+                
+                Section(header: Text("How many questions you want to be asked?")) {
+                    Picker("", selection: $selectedQuestionsCount) {
+                        ForEach (0..<questions.count) { index in
+                            Text("\(self.questions[index])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
             }
-        .navigationBarTitle("Edutainment")
+            .navigationBarTitle("Edutainment")
         }
     }
 }
