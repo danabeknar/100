93577a22fe0501ec020d9472ce3c45d173a712ec
Beknar Danabek <beknar.danabek@gmail.com>
2020-08-28 00:12:06 +0600

    feat(day 39): pushing new views onto the stack using NavigationLink

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,26 +8,15 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        self.text = text
-        print("Creating a custom text")
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        List {
-            ForEach(0..<100) {
-                CustomText("Item \($0)")
-                    .font(.title)
+        NavigationView {
+            List(0..<100) { row in
+//                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
+//                }
             }
+        .navigationBarTitle("SwiftUI")
         }
     }
 }
