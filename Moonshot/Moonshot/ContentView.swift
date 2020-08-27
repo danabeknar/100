01bef3281c0def4047f48ef472eb9cbcf0956024
Beknar Danabek <beknar.danabek@gmail.com>
2020-08-27 23:39:40 +0600

    feat(day 39): Moonshot: Introduction

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Beknar Danabek on 8/27/20.
+//  Copyright © 2020 Beknar Danabek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
