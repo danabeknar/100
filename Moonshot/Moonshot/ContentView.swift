ebd89ea70503a2c5f1e1579f33eec85a5e8a3d31
Beknar Danabek <beknar.danabek@gmail.com>
2020-08-27 23:51:31 +0600

    feat(day 39): resizing images to fit the screen using GeometryReader

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            GeometryReader { geo in
+                Image("Example")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geo.size.width)
+            }
+        }
     }
 }
 
