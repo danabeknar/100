ce359ed7c32a5b1db1039b172b440dd958fd3e94
Beknar Danabek <beknar.danabek@gmail.com>
2020-08-29 22:09:51 +0600

    feat(day 40): formatting our mission view

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -13,7 +13,23 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Destination View")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+        .navigationBarTitle("Moonshot")
+        }
     }
 }
 
