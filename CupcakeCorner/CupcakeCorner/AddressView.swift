5b0e0e63a47c97c41315b857a9e6486ff1934057
Beknar Danabek <beknar.danabek@gmail.com>
2020-09-13 12:50:57 +0300

    feat(day 50): checking for a valid address

--- CupcakeCorner/CupcakeCorner/AddressView.swift
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -12,7 +12,21 @@ struct AddressView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink(destination: CheckoutView(order: order)) {
+                    Text("Check out")
+                }.disabled(order.hasValidAddress == false)
+            }
+        }
+        .navigationBarTitle("Delivery details", displayMode: .inline)
     }
 }
 
