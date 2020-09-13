e92228f21ac8136f9404e3ad85d9788373fe35c9
Beknar Danabek <beknar.danabek@gmail.com>
2020-09-13 22:08:04 +0300

    feat(day 53): Bookworm: Introduction

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Beknar Danabek on 9/13/20.
+//  Copyright © 2020 Beknar Danabek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
