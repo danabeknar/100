16bc76e3e76554e17c00a91e59f550a01cb48665
Beknar Danabek <beknar.danabek@gmail.com>
2020-09-15 19:52:10 +0300

    feat(day 54): building a list with @FetchRequest

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -16,8 +16,22 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            Text("Count \(books.count)")
-                .navigationBarTitle("Bookworm")
+            List {
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: Text(book.title ?? "Unknown title")) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Bookworm")
             .navigationBarItems(trailing: Button(action: {
                 self.showingAddScreen.toggle()
             }) {
