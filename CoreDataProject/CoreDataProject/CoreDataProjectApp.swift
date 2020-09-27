2a7a916d28eeac77f68f8abc1cb7c492566ce735
Beknar Danabek <beknar.danabek@gmail.com>
2020-09-27 21:50:55 +0300

    feat(day 57): reating NSManagedObject subclasses

--- CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -9,12 +9,8 @@ import SwiftUI
 
 @main
 struct CoreDataProjectApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Beknar Danabek on 9/27/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
