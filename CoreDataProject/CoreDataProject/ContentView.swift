76aedce485875b9811b464c3581717fbc8c6e309
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-03 19:07:08 +0300

    feat(day 58): challenges

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -14,7 +14,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            FilteredList(filter: lastNameFilter)
+            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            }
             
             Button("Add Examples") {
                 let taylor = Singer(context: moc)
--- CoreDataProject/CoreDataProject/FilteredList.swift
+++ CoreDataProject/CoreDataProject/FilteredList.swift
@@ -6,23 +6,34 @@
 //
 
 import SwiftUI
+import CoreData
 
-struct FilteredList: View {
-    var fetchRequest: FetchRequest<Singer>
-    
-    init(filter: String) {
-        fetchRequest = FetchRequest<Singer>(entity: Singer.entity(), sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
-    }
-    
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    var fetchRequest: FetchRequest<T>
+    var singers: FetchedResults<T> { fetchRequest.wrappedValue }
+
+    // this is our content closure; we'll call this once for each item in the list
+    let content: (T) -> Content
+
     var body: some View {
         List(fetchRequest.wrappedValue, id: \.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            self.content(singer)
         }
+    }
+
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        fetchRequest = FetchRequest<T>(entity: T.entity(),
+                                       sortDescriptors: [],
+                                       predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
 
 struct FilteredList_Previews: PreviewProvider {
     static var previews: some View {
-        FilteredList(filter: "A")
+        FilteredList(filterKey: "lastName",
+                     filterValue: "A") { (singer: Singer) in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
     }
 }
