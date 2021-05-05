a1462389191705e7984eb584f88f7d7a750560df
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-05 10:26:52 +0300

    feat(day 62): instafilter introduction

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Beknar Danabek on 5.05.21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
