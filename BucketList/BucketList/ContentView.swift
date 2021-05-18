0f5f1622dfb3916ca67c183b34d6a5c98a3a1e61
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-18 23:18:57 +0300

    feat(day 68): writing data to the documents directory

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -15,9 +15,28 @@ struct ContentView: View {
     ].sorted()
     
     var body: some View {
-        List(users) { user in
-                    Text("\(user.lastName), \(user.firstName)")
+        Text("Hello World")
+            .onTapGesture {
+                let str = "Test Message"
+                
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
                 }
+            }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
     }
 }
 
