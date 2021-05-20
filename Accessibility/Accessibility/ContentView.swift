cc38b5dbf00b2ae83fb4007cea13eea357c3c7f1
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-20 16:50:22 +0300

    feat(day 74): reading the value of controls

--- Accessibility/Accessibility/ContentView.swift
+++ Accessibility/Accessibility/ContentView.swift
@@ -8,32 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-            "ales-krivec-15949",
-            "galina-n-189483",
-            "kevin-horstmann-141705",
-            "nicolas-tissot-335096"
-        ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks",
-    ]
+    @State private var rating = 3
 
-    @State private var selectedPicture = Int.random(in: 0...3)
-
-        var body: some View {
-            VStack {
-                Text("Your score is")
-                Text("1000")
-                    .font(.title)
-            }
-            
-            .accessibilityElement(children: .ignore)
-            .accessibility(label: Text("Your score is 1000"))
-        }
+    var body: some View {
+        Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+            .accessibility(value: Text("\(rating) out of 5"))
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
