c0a2b73743319886b77d1b4d65ba2f530ed12737
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-20 16:25:54 +0300

    feat(day 74): accessibility introduction

---
+++ Accessibility/Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Beknar Danabek on 20.05.21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
