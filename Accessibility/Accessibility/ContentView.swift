ad192cd5833191b422d6ba4eef40c9169ff39236
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-20 16:40:13 +0300

    feat(day 74): identifying views with useful labels

--- Accessibility/Accessibility/ContentView.swift
+++ Accessibility/Accessibility/ContentView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    let pictures = [
+            "ales-krivec-15949",
+            "galina-n-189483",
+            "kevin-horstmann-141705",
+            "nicolas-tissot-335096"
+        ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+
+    @State private var selectedPicture = Int.random(in: 0...3)
+
+        var body: some View {
+            Image(pictures[selectedPicture])
+                .resizable()
+                .scaledToFit()
+                .accessibility(label: Text(labels[selectedPicture]))
+                .accessibility(addTraits: .isButton)
+                .onTapGesture {
+                    self.selectedPicture = Int.random(in: 0...3)
+                }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
