d58d3d71d29be10a448901ec546e33a3e21dacb8
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-22 21:31:25 +0300

    feat(day 80): understanding Swift’s Result type

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,28 +7,52 @@
 
 import SwiftUI
 
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
+
 struct ContentView: View {
     @State private var selectedTab = 0
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    self.selectedTab = 1
+        Text("Hello, World!")
+            .onAppear {
+                self.fetchData(from: "https://www.apple.com") { result in
+                    switch result {
+                    case .success(let str):
+                        print(str)
+                    case .failure(let error):
+                        switch error {
+                        case .badURL:
+                            print("Bad URL")
+                        case .requestFailed:
+                            print("Network problems")
+                        case .unknown:
+                            print("Unknown error")
+                        }
+                    }
                 }
-                .tabItem {
-                    Image(systemName: "star")
-                    Text("One")
-                }
-                .tag(0)
-
-            Text("Tab 2")
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Two")
-                }
-                .tag(1)
+            }
+    }
+    
+    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
         }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    completion(.failure(.requestFailed))
+                } else {
+                    completion(.failure(.unknown))
+                }
+            }
+        }.resume()
     }
 }
 
