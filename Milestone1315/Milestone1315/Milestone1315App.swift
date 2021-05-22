6426977cfea086f62db29c39b7c8f3e753507eec
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-22 14:04:13 +0300

    feat(day 77): init project

---
+++ Milestone1315/Milestone1315/Milestone1315App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone1315App.swift
+//  Milestone1315
+//
+//  Created by Beknar Danabek on 22.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Milestone1315App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
