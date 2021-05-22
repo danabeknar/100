316868baac809a9e1185671a02cf1dcf9b8b7cda
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-22 21:09:39 +0300

    feat(day 79): Hot Prospects

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Beknar Danabek on 22.05.21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
