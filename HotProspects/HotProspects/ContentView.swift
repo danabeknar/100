f97e8a3daaeb5cfecceaa0738991452a458e085b
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-22 21:42:35 +0300

    feat(day 80): manually publishing ObservableObject changes

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,52 +7,27 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedTab = 0
+    @ObservedObject var updater = DelayedUpdater()
 
     var body: some View {
-        Text("Hello, World!")
-            .onAppear {
-                self.fetchData(from: "https://www.apple.com") { result in
-                    switch result {
-                    case .success(let str):
-                        print(str)
-                    case .failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("Bad URL")
-                        case .requestFailed:
-                            print("Network problems")
-                        case .unknown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-            }
-    }
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    completion(.failure(.requestFailed))
-                } else {
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
+        Text("Value is: \(updater.value)")
     }
 }
 
