62c1cef9d1ef7f58fc67f58d09a832e3caea5d12
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-23 20:00:07 +0300

    feat(day 90): adding and deleting cards

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -18,83 +18,47 @@ extension View {
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @Environment(\.accessibilityEnabled) var accessibilityEnabled
-    
-    @State private var cards = [Card](repeating: Card.example, count: 10)
+
+    @State private var cards = [Card]()
+    @State private var showingEditScreen = false
+
     @State private var timeRemaining = 100
     @State private var isActive = true
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    
+
     var body: some View {
         ZStack {
             Image(decorative: "background")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
-            
+
             VStack {
                 Text("Time: \(timeRemaining)")
                     .font(.largeTitle)
                     .foregroundColor(.white)
                     .padding(.horizontal, 20)
-                    .padding(.vertical, 20)
+                    .padding(.vertical, 5)
                     .background(
                         Capsule()
                             .fill(Color.black)
                             .opacity(0.75)
                     )
-                
-                if differentiateWithoutColor || accessibilityEnabled {
-                    VStack {
-                        Spacer()
 
-                        HStack {
-                            Button(action: {
-                                withAnimation {
-                                    self.removeCard(at: self.cards.count - 1)
-                                }
-                            }) {
-                                Image(systemName: "xmark.circle")
-                                    .padding()
-                                    .background(Color.black.opacity(0.7))
-                                    .clipShape(Circle())
-                            }
-                            .accessibility(label: Text("Wrong"))
-                            .accessibility(hint: Text("Mark your answer as being incorrect."))
-                            Spacer()
-
-                            Button(action: {
-                                withAnimation {
-                                    self.removeCard(at: self.cards.count - 1)
-                                }
-                            }) {
-                                Image(systemName: "checkmark.circle")
-                                    .padding()
-                                    .background(Color.black.opacity(0.7))
-                                    .clipShape(Circle())
-                            }
-                            .accessibility(label: Text("Correct"))
-                            .accessibility(hint: Text("Mark your answer as being correct."))
-                        }
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .padding()
-                    }
-                }
-                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
-                            withAnimation {
-                                removeCard(at: index)
-                            }
+                           withAnimation {
+                               removeCard(at: index)
+                           }
                         }
-                            .stacked(at: index, in: cards.count)
+                        .stacked(at: index, in: cards.count)
                         .allowsHitTesting(index == cards.count - 1)
                         .accessibility(hidden: index < cards.count - 1)
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
-                
+
                 if cards.isEmpty {
                     Button("Start Again", action: resetCards)
                         .padding()
@@ -103,39 +67,108 @@ struct ContentView: View {
                         .clipShape(Capsule())
                 }
             }
-        }
-        .onReceive(timer) { time in
-            guard self.isActive else { return }
-            
-            if self.timeRemaining > 0 {
-                self.timeRemaining -= 1
-            }
 
+            VStack {
+                HStack {
+                    Spacer()
+
+                    Button(action: {
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                }
+
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
+
+            if differentiateWithoutColor || accessibilityEnabled {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Button(action: {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "xmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Wrong"))
+                        .accessibility(hint: Text("Mark your answer as being incorrect."))
+                        Spacer()
+
+                        Button(action: {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "checkmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Correct"))
+                        .accessibility(hint: Text("Mark your answer as being correct."))
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-            self.isActive = false
+            isActive = false
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-            if self.cards.isEmpty == false {
-                self.isActive = true
+            if cards.isEmpty == false {
+               isActive = true
             }
         }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards) {
+            EditCards()
+        }
+        .onAppear(perform: resetCards)
     }
-    
+
     func removeCard(at index: Int) {
         guard index >= 0 else { return }
-        
+
         cards.remove(at: index)
-        
+
         if cards.isEmpty {
             isActive = false
         }
     }
-    
+
     func resetCards() {
-        cards = [Card](repeating: Card.example, count: 10)
         timeRemaining = 100
         isActive = true
+        loadData()
+    }
+
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
     }
 }
 
