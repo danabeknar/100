6a6945dbf37dc4fe832b1c380cfbbc5bdbd5d8eb
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-23 17:22:20 +0300

    feat(day 86): flashzilla introduction

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Beknar Danabek on 23.05.21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
