d55c1b93888175f915a7bd9b083706cb05e2a2b5
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-23 18:16:19 +0300

    feat(day 87): triggering events repeatedly using a timer

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -10,17 +10,20 @@ import CoreHaptics
 
 struct ContentView: View {
     @State private var engine: CHHapticEngine?
-    
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
-        }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            print("VStack tapped!")
-        }
+        Text("Hello, World!")
+            .onReceive(timer) { time in
+                if self.counter == 5 {
+                    self.timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+
+                self.counter += 1
+            }
     }
     
     func simpleSuccess() {
