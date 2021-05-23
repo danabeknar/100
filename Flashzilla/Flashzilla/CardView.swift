8e4268cc9b837583846ed773ecf7f38c6757a87c
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-23 18:55:34 +0300

    feat(day 88): moving views with DragGesture and offset()

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
+    
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -32,6 +35,23 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                }
+            
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        self.offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
