e54ab653d21ddf08995da2754b813e8bfa328102
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-23 17:32:30 +0300

    feat(day 86): how to use gestures in SwiftUI

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+
+    // whether it is currently being dragged or not
+    @State private var isDragging = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in self.offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    self.offset = .zero
+                    self.isDragging = false
+                }
+            }
+
+        // a long press gesture that enables isDragging
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    self.isDragging = true
+                }
+            }
+
+        // a combined gesture that forces the user to long press then drag
+        let combined = pressGesture.sequenced(before: dragGesture)
+
+        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
