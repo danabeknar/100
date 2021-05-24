836c9895d83813c41a024f31662eae4c592fccd5
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-24 21:01:35 +0300

    feat(day 92): layout and geometry

---
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Beknar Danabek on 24.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Beknar Danabek on 24.05.21.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
