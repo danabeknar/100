14d5f3b8888704ec7bfb9dd79f17408f37ed49d8
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-24 21:33:38 +0300

    feat(day 93): absolute positioning for SwiftUI views

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -19,27 +19,9 @@ extension VerticalAlignment {
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("background")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                Text("Test")
-                Text("Test")
-                Text("Test")
-                Text("Test")
-                Text("Test")
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+            .background(Color.red)
+            .offset(x: 100, y: 100)
     }
 }
 
