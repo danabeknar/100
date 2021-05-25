e3b0e0e17323a3e000e0c6915fc5673ffbe67b47
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-26 00:10:28 +0300

    feat(day 96): using groups as transparent layout containers

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -11,19 +11,27 @@ struct User: Identifiable {
     var id = "Taylor Swift"
 }
 
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna, Arya, and Toby")
+        }
+    }
+}
+
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingAlert = false
+    @Environment(\.horizontalSizeClass) var sizeClass
 
     var body: some View {
-        Text("Hello, World!")
-            .onTapGesture {
-                self.selectedUser = User()
-                self.isShowingAlert = true
+        Group {
+            if sizeClass == .compact {
+                VStack(content: UserView.init)
+            } else {
+                HStack(content: UserView.init)
             }
-            .alert(isPresented: $isShowingAlert) {
-                Alert(title: Text(selectedUser!.id))
-            }
+        }
     }
 }
 
