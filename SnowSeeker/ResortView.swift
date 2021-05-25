64fbbf59d87494ac303e00110a3428ff53b33955
Beknar Danabek <beknar.danabek@gmail.com>
2021-05-26 00:40:59 +0300

    feat(day 97): creating a secondary view for NavigationView

---
+++ SnowSeeker/ResortView.swift
@@ -0,0 +1,52 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Beknar Danabek on 26.05.21.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    let resort: Resort
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    Spacer()
+                    ResortDetailsView(resort: resort)
+                    SkiDetailsView(resort: resort)
+                    Spacer()
+                }
+                .font(.headline)
+                .foregroundColor(.secondary)
+                .padding(.top)
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Facilities")
+                        .font(.headline)
+                    
+                    Text(ListFormatter.localizedString(byJoining: resort.facilities))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationBarTitle(Text("\(resort.name), \(resort.country)"),
+                            displayMode: .inline)
+    }
+}
+
+struct ResortView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortView(resort: Resort.example)
+    }
+}
